f761455aa9d775439fd4d94c7396679813c5105b
zianazhao <zianazhao@tencent.com>
2023-02-02 15:57:12 +0800

    fix-timeZone

--- pkg/util/time.go
+++ pkg/util/time.go
@@ -6,10 +6,11 @@ import "time"
 func FormatTime(t time.Time, format string) string {
 	var local time.Time
 	_, offset := t.Zone()
-	if offset == 0 {
-		local = t.Add(8 * time.Hour)
-	} else {
-		local = t
-	}
+	// if offset == 0 {
+	// 	local = t.Add(8 * time.Hour)
+	// } else {
+	// 	local = t
+	// }
+	local = t.Add(time.Duration(8*3600-offset) * time.Second)
 	return local.Format(format)
 }
